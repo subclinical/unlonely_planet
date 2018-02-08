72f376cc64ca48fa58d98f4a04c3435f47afdc34
michaelc <michael.38@outlook.com>
2018-02-07 21:43:19 -0500

    map auto zooms/centers when rendering multiple locations

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,18 +17,75 @@ $(document).ready(function () {
   // on mouseleave, hide marker's infoWindow
 
 
+  let sampleCoords = [
+    {
+      lat: 51.507,
+      lng: -0.128
+    },
+    {
+      lat: 51.663,
+      lng: -0.831
+    },
+    {
+      lat: 55.501,
+      lng: -2.333
+    }
+  ]
 
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
+  var map;
+  bounds = new google.maps.LatLngBounds();
 
-function createPointElement(obj) {
-  let point_description = obj.description;
-  let point_location = obj.location;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 51.5074, lng: -0.1278 },
+      zoom: 8
+    });
+  }
 
-  pointElement = (`
+  function renderMarkers(array) {
+    // console.log(array)
+    for (coords of array) {
+      createMarker(coords) // create a new marker
+      loc = new google.maps.LatLng(marker.position.lat(), marker.position.lng()); // based on the newly created marker, extend the "bounds"
+      bounds.extend(loc);
+    }
+    map.fitBounds(bounds);
+  }
+
+  function createMarker(coords) {
+    marker = new google.maps.Marker({
+      position: coords,
+      map: map
+    });
+  }
+  // map.fitBounds(bounds);
+
+  initMap();
+  renderMarkers(sampleCoords);
+
+
+
+  // var bounds = new.google.maps.LatLngBounds();
+  // console.log(bounds);
+  // for (let i = 0; i < sampleCoords.length; i++) {
+  //   bounds.extend(sampleCoords[i].getPosition());
+  // }
+
+  // map.fitBounds(bounds);
+
+
+
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  function createPointElement(obj) {
+    let point_description = obj.description;
+    let point_location = obj.location;
+
+    pointElement = (`
   
   <article class="point_element">
   <!-- <img class="location_pic" src="/images/location.png"> -->
@@ -36,32 +93,33 @@ function createPointElement(obj) {
   <span class="point_element_location"> ${escape(point_location)} </span>
   </article>
   `);
-  console.log(pointElement);
-  return pointElement;
-};
+
+    return pointElement;
+  };
 
 
-function renderPoints(array) {
-  console.log("renderPoints function invoked")
-  for (point of array) { // point is an object within an array
-    console.log(point);
-    $(".point_element_container").append(createPointElement(point));
+  function renderPoints(array) {
+
+    for (point of array) { // point is an object within an array
+
+      $(".point_element_container").append(createPointElement(point));
+    }
   }
-}
 
 
-let sample = [
-  {
-    description: "Palace2",
-    location: "London"
-  },
-  {
-    description: "Waterfall",
-    location: "Sydney"
-  }
-];
+  let samplePoints = [
+    {
+      description: "Palace2",
+      location: "London"
+    },
+    {
+      description: "Waterfall",
+      location: "Sydney"
+    }
+  ];
 
-renderPoints(sample);
+  renderPoints(samplePoints);
+
 
 
 })
