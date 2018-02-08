a8cbb25d976199ee5817b040188cc07945ba070f
brandonstranzl <brandonstranzl@gmail.com>
2018-02-08 01:27:32 +0000

    new origin commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,28 +17,37 @@ $(document).on('ready', function () {
 // marker.getLatLng(); returns the latitude and longitude of the marker,
 // while map.panTo(GLatLng) allows us to center the map on that latitude and longitude.
 
-var loadMaps = function () {
+// var loadMaps = function () {
 
-$.ajax({
-  url: '/maps/:id',
-  method: 'GET',
-  success: function (response) {
-    console.log('Success: ', response);
-    $('#maps-container').empty();
-    renderMap(response);
-    }
-  })
+// $.ajax({
+//   url: '/maps/:id',
+//   method: 'GET',
+//   success: function (response) {
+//     console.log('Success: ', response);
+//     $('#maps-container').empty();
+//     renderMap(response);
+//     }
+//   })
 
-}
+// }
 
 var renderMap = function (response) {
   function initMap() {
   var options = {
-  zoom: response.zoom,
-  center: {lat: response.lat, lng: response.lng }
+  zoom: 10,
+  center: {lat: mapinfo.lat, lng: mapinfo.lng }
   }
 
   var map = new google.maps.Map(document.getElementById('map'), options);
+
+  var markers = for (var i of mapinfo);//some array
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < markers.length; i++) {
+  bounds.extend(markers[i].getPosition());
+  }
+
+map.fitBounds(bounds);
+
 
   var bounds  = new google.maps.LatLngBounds();
   loc = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
@@ -49,9 +58,9 @@ var renderMap = function (response) {
   }
 }
 
+renderMap(mapinfo);
 
-
-});
+// });
 
 
 
@@ -72,12 +81,29 @@ var renderMap = function (response) {
 
 
 
-mapinfo = {
+var mapinfo = [{
   id: 1,
-  title: New York,
-  lat:
-  lng:
+  title: Fresh on Spadina,
+  lat: 43.648239,
+  lng: -79.395851
   zoom:
   date_created:
   creator_id:
-}
+},
+{ id: 2,
+  title: Ikea,
+  lat: 43.618487,
+  lng: -79.534538
+  zoom:
+  date_created:
+  creator_id:
+},
+{ id: 4,
+  title: Ikea,
+  lat: 50.120578,
+  lng: -122.957455
+  zoom:
+  date_created:
+  creator_id:
+}];
+
