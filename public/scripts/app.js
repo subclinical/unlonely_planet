1de5a14d7f0fd16623d21633bd9f92a54758242a
brandonstranzl <brandonstranzl@gmail.com>
2018-02-08 21:42:32 +0000

    recent api function - init

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,19 +30,25 @@
   // center: {lat: mapinfo.lat, lng: mapinfo.lng }
   // }
 
-  var maps = { markers: [{
-  lat: 43.648239,
-  lng: -79.395851,
-  description: "this is a test description"
-  },
-{
-  lat: 46.618487,
-  lng: -79.534538,
-},
-{
-  lat: 50.120578,
-  lng: -122.957455,
-}] };
+  var maps = {
+    markers: [
+      {
+        lat: 43.648239,
+        lng: -79.395851,
+        description: "this is a test description"
+        },
+      {
+        lat: 46.618487,
+        lng: -79.534538,
+        description: "b"
+      },
+      {
+        lat: 50.120578,
+        lng: -122.957455,
+        description: "c"
+      }
+    ]
+  };
 
   mylatLng = { lat: maps.markers[0].lat, lng: maps.markers[0].lng }
 
@@ -56,20 +62,26 @@
 
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  for (i = 0; i < maps.markers.length; i++) {
-    var position = new google.maps.LatLng(maps.markers[i].lat, maps.markers[i].lng);
-    bounds.extend(position);
-      var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-    });
-      var infoWindow = new google.maps.InfoWindow({ content: maps.markers.description });
-        // google.maps.event.addListener(marker, 'click', function(event){
-  marker.addListener('click', function () {
-  infoWindow.open(map, marker);
-  console.log("clicked!")
-  });
+  for (var i = 0; i < maps.markers.length; i++) {
 
+    (function(i) {
+
+      var position = new google.maps.LatLng(maps.markers[i].lat, maps.markers[i].lng);
+      bounds.extend(position);
+
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+      });
+
+      marker.addListener('click', function () {
+        console.log(position.lat)
+        var infoWindow = new google.maps.InfoWindow({ content: maps.markers[i].description, position: position});
+        infoWindow.open(map, marker);
+        console.log("clicked!")
+      });
+
+    })(i)
 
   }
 
@@ -77,21 +89,24 @@
   map.panToBounds(bounds);     // # auto-center
 
   // listen for a click and run function addMarker on a click:
-  // google.maps.event.addListener(map, 'click',
-  //   function(event){
-  //   console.log("clicked")
-  //   addMarker({coords: event.latLng});
-  //   console.log(event.latLng)
-  // });
+  google.maps.event.addListener(map, 'click',
+    function(event){
+    console.log("clicked")
+    addMarker({coords: event.latLng});
+    console.log(event.latLng)
+  });
 
   function addMarker(props) {
     var marker = new google.maps.Marker({
-    position: props.coords,
-    map:map,
+      position: props.coords,
+      map:map,
     });
+    var infoWindow = new google.maps.InfoWindow({ content: "blah", position: props.coords});
+    marker.addListener('click', function () {
+      infoWindow.open(map, marker);
+    })
+
   }
-
-
 
 
 }
