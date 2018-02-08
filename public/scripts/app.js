326f04e829ada5edc4bd7c33c06665fb0f65ead9
brandonstranzl <brandonstranzl@gmail.com>
2018-02-08 18:38:33 +0000

    api commit - added event listeners for markers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,15 +11,15 @@
 
 // var loadMaps = function () {
 
-$.ajax({
-  url: '/maps/:id',     123456
-  method: 'GET',
-  success: function (response) {
-    console.log('Success: ', response);
-    $('#maps-container').empty();
-    renderMap(response);
-    }
-  })
+// $.ajax({
+//   url: '/maps/:id',     123456
+//   method: 'GET',
+//   success: function (response) {
+//     console.log('Success: ', response);
+//     $('#maps-container').empty();
+//     renderMap(response);
+//     }
+//   })
 
 // }
 
@@ -30,22 +30,23 @@ $.ajax({
   // center: {lat: mapinfo.lat, lng: mapinfo.lng }
   // }
 
-  var markers = [{
+  var maps = { markers: [{
   lat: 43.648239,
   lng: -79.395851,
+  description: "this is a test description"
   },
 {
-  lat: 43.618487,
+  lat: 46.618487,
   lng: -79.534538,
 },
 {
   lat: 50.120578,
   lng: -122.957455,
-}];
+}] };
 
-  mylatLng = { lat: markers[0].lat, lng: markers[0].lng }
+  mylatLng = { lat: maps.markers[0].lat, lng: maps.markers[0].lng }
 
-  function initMap(maps) {
+  function initMap() {
 
   var mapOptions = {
     center: mylatLng // this is from mapID
@@ -55,17 +56,43 @@ $.ajax({
 
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  for (i = 0; i < markers.length; i++) {
+  for (i = 0; i < maps.markers.length; i++) {
     var position = new google.maps.LatLng(maps.markers[i].lat, maps.markers[i].lng);
     bounds.extend(position);
       var marker = new google.maps.Marker({
       position: position,
       map: map,
     });
+      var infoWindow = new google.maps.InfoWindow({ content: maps.markers.description });
+        // google.maps.event.addListener(marker, 'click', function(event){
+  marker.addListener('click', function () {
+  infoWindow.open(map, marker);
+  console.log("clicked!")
+  });
+
+
   }
 
   map.fitBounds(bounds);       //# auto-zoom
   map.panToBounds(bounds);     // # auto-center
+
+  // listen for a click and run function addMarker on a click:
+  // google.maps.event.addListener(map, 'click',
+  //   function(event){
+  //   console.log("clicked")
+  //   addMarker({coords: event.latLng});
+  //   console.log(event.latLng)
+  // });
+
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+    position: props.coords,
+    map:map,
+    });
+  }
+
+
+
 
 }
 
