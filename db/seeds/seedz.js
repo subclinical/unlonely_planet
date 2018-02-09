33459824f628fa3c5041483abf49ed1264f1c8d3
Danny K Pan <kaiwen006@gmail.com>
2018-02-09 22:28:42 +0000

    updated pictures to correspond to their locations

--- db/seeds/seedz.js
+++ db/seeds/seedz.js
@@ -20,68 +20,44 @@ exports.seed = function(knex, Promise) {
         knex('maps').insert({
           id: 1,
           title: 'Places in Toronto Worth Getting on the Public Transit for',
-          lat: 43.6532,
-          lng: -79.3832,
-          zoom: 8,
           user_key: '012345',
           date_created: new Date(),
-          city: 'Toronto',
           image: 'https://intersectionsmatch.com/wp-content/uploads/2014/09/great-love-debate-toronto-2.jpg'
         }),
         knex('maps').insert({
           id: 2,
           title: 'Hot Spots in Paris',
-          lat: 48.8566,
-          lng: 2.3522,
-          zoom: 8,
           user_key: '012345',
           date_created: new Date(),
-          city: 'Paris',
           image: 'https://pictures.tripmasters.com/images/packages/france/paris-eiffeltowergrass200.jpg'
         }),
         knex('maps').insert({
           id: 3,
           title: 'Cool Places in Shanghai',
-          lat: 31.230390,
-          lng: 121.473702,
-          zoom: 8,
           user_key: '012345',
           date_created: new Date(),
-          city: 'Shanghai',
           image: 'https://pictures.tripmasters.com/images/packages/china/shanghai-citynightview200.jpg'
         }),
         knex('maps').insert({
           id: 4,
           title: 'Everybody loves Vancouver',
-          lat: 0,
-          lng: 0,
-          zoom: 8,
           user_key: '012345',
           date_created: new Date(),
-          city: 'Vancouver',
-          image: 'https://pictures.tripmasters.com/images/packages/china/shanghai-citynightview200.jpg'
+          image: 'http://centreforinquiry.ca/wp-content/uploads/2013/05/vancouver-skyline-200x200.jpg'
         }),
         knex('maps').insert({
           id: 5,
           title: 'Restaurants in Rio de Janeiro',
-          lat: -22.906847,
-          lng: -43.172896,
-          zoom: 8,
           user_key: '012345',
           date_created: new Date(),
-          city: 'Rio de Janeiro',
-          image: 'https://pictures.tripmasters.com/images/packages/china/shanghai-citynightview200.jpg'
+          image: 'http://masterbedroomideas.eu/wp-content/uploads/2016/08/1243848-december-26-2015-rio-de-janeiro-travel-image-galleries-200x200.jpg'
         }),
         knex('maps').insert({
           id: 6,
           title: 'Come Here During the Offseason, Otherwise Crowding is an Issue',
-          lat: -62.940930,
-          lng: -60.555375,
-          zoom: 8,
           user_key: '012345',
           date_created: new Date(),
-          city: 'South Pole',
-          image: 'https://pictures.tripmasters.com/images/packages/china/shanghai-citynightview200.jpg'
+          image: 'https://www.keele.ac.uk/media/keeleuniversity/generalimagebank/200x200px/antarctica-200x200.jpg'
         })
       ]);
     })
@@ -103,7 +79,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Where productivity and soundproof flooring are mutually exclusive.',
-          image: 'https://www.fillmurray.com/100/100'
+          image: 'https://www.bcjobs.ca/employerlogo/2DCF2E88-EBC7-B6F3-0546D8FC52B3C372.png?width=350&mode=max',
+          city: 'Toronto'
         }),
         knex('markers').insert({
           id: 2,
@@ -114,7 +91,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Eiffel like a lot of people come here for sightseeing.',
-          image: 'https://www.fillmurray.com/100/100'
+          image: 'https://igx.4sqi.net/img/general/200x200/818134_yqfoDK7aaekqXDyiIgz4BPCND6dPvvagL1c6hOQkKVQ.jpg',
+          city: 'Paris'
         }),
         knex('markers').insert({
           id: 3,
@@ -125,7 +103,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'New way to experience people surfing.',
-          image: 'https://www.fillmurray.com/100/100'
+          image: 'https://files2.coloribus.com/files/companies/part_16/167505/mcdonalds-167505-1493289558_logo.png',
+          city: 'Shanghai'
         }),
         knex('markers').insert({
           id: 4,
@@ -136,7 +115,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Rotating restaurant only cater to those who use the stairs',
-          image: 'https://www.fillmurray.com/100/100'
+          image: 'https://c.ymcdn.com/sites/www.strategyassociation.org/resource/resmgr/2017_conference/CNtower.png',
+          city: 'Toronto'
         }),
         knex('markers').insert({
           id: 5,
@@ -147,7 +127,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Cool place to loiter.',
-          image: 'https://www.fillmurray.com/100/100'
+          image: 'https://igx.4sqi.net/img/general/200x200/48239911_1SNsnIegO_oGlMW5QkRPpqUuE8ewU2vQxYcy9L2GWT8.jpg',
+          city: 'Toronto'
         }),
         knex('markers').insert({
           id: 6,
@@ -158,7 +139,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Let taste buds monopolize your dining experience for a night.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://b.zmtcdn.com/data/pictures/7/8910537/fa04e5856e0fdac25a6f215a05a39aa7.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
+          city: 'Toronto'
         }),
         knex('markers').insert({
           id: 7,
@@ -169,7 +151,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Get edumacated.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/39373183_-fnvwd-txdOWx1ED7vf88ICxxINc9pZgNHdlp3amDHw.jpg',
+          city: 'Toronto'
         }),
         knex('markers').insert({
           id: 8,
@@ -180,7 +163,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Bro, the sharks here are so t h i c c, come look.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'http://thesurge.com/wp-content/uploads/2016/02/Ripleys-Aquarium-Of-The-Smokies-59357-200x200.jpg',
+          city: 'Toronto'
         }),
         knex('markers').insert({
           id: 9,
@@ -191,7 +175,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Spend your life savings here in the blink of an eye.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/348213_1oQ4lNa8TwZu617pqPPBQjgkypv7yFxSzhQMffXEue8.jpg',
+          city: 'Toronto'
         }),
         knex('markers').insert({
           id: 10,
@@ -202,7 +187,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Da Vinci lives here.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/41168100_l2WP9NmifL3JAKFmZajmmc1v3g23M5CIJIsVvTynNa0.jpg',
+          city: 'Paris'
         }),
         knex('markers').insert({
           id: 11,
@@ -213,7 +199,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Because there are no Disneylands anywhere else.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://i2.wp.com/www.dlptoday.com/images/2017/02/N024534_BD-01-1200x577.png?resize=200%2C200',
+          city: 'Paris'
         }),
         knex('markers').insert({
           id: 12,
@@ -224,7 +211,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'I think this is a church with a gothic design.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://ph.olobahataahu.info/small/200/thumbnail/ANd9GcTsTwOv3ztLUufRUkz3ABz4WLNYu6wip3FCAMkuNouv9i9LFdCD0w.jpg',
+          city: 'Paris'
         }),
         knex('markers').insert({
           id: 13,
@@ -235,7 +223,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Hey, we got something tall too.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://cache-graphicslib.viator.com/graphicslib/6139/SITours/dinner-at-the-oriental-pearl-tower-revolving-restaurant-with-transfers-in-shanghai-311976.jpg',
+          city: 'Shanghai'
         }),
         knex('markers').insert({
           id: 14,
@@ -246,7 +235,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Come feed our fishes, for just 3 installments of $13.99 per fish.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'http://www.chinafacttours.com/images/shanghai/yu-garden/yu-garden-704261.jpg.thumb.jpg',
+          city: 'Shanghai'
         }),
         knex('markers').insert({
           id: 15,
@@ -257,7 +247,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Hipster gatherings, come only if hip.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/4368853_4TGv3Sz1t6oU065pNBQSaAYx_WOB2CKFk0H6b9FYBfQ.jpg',
+          city: 'Vancouver'
         }),
         knex('markers').insert({
           id: 16,
@@ -268,7 +259,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Wow trees.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/10225207_7pV2Plei-G-mSho9Xz92JkJYZa-N9K4xKU9Jnw43wrA.jpg',
+          city: 'Vancouver'
         }),
         knex('markers').insert({
           id: 17,
@@ -279,7 +271,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Climb me.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/48613774_x3A2qnYDwzUuSnKNi5-qRx8kOHE30D-UArpdbKmuoMY.jpg',
+          city: 'Vancouver'
         }),
         knex('markers').insert({
           id: 18,
@@ -290,7 +283,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'French restaurant in Rio.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/22156095_H9Tpdhd0B2Uv5-k09kVSTCB2e9Wniza9qmmj8QLxqXw.jpg',
+          city: 'Rio de Janeiro'
         }),
         knex('markers').insert({
           id: 19,
@@ -301,7 +295,8 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Elegant setting.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://b.zmtcdn.com/data/pictures/2/7301112/54b1a93b6abee60d7f22a3991f593d93.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
+          city: 'Rio de Janeiro'
         }),
         knex('markers').insert({
           id: 20,
@@ -312,8 +307,21 @@ exports.seed = function(knex, Promise) {
           user_key: '012345',
           date_created: new Date(),
           description: 'Another French restaurant in Rio.',
-          image: 'https://www.fillmurray.com/110/110'
+          image: 'https://igx.4sqi.net/img/general/200x200/72271806__PxoZiBT4nksxjwIEctRa0fDalC26oGi3C6aRn0lQLA.jpg',
+          city: 'Rio de Janeiro'
         }),
+        knex('markers').insert({
+          id: 21,
+          map_id: 6,
+          label: 'McDonald\'s',
+          lat: -62.940930,
+          lng: -60.555375,
+          user_key: '012345',
+          date_created: new Date(),
+          description: 'Breakfast items now available all day!',
+          image: 'http://sdmcdonalds.com/wp-content/uploads/2015/04/all-day-breakfast.png',
+          city: 'Deception Island'
+        })
       ])
       .then( () => {
         return Promise.all([
